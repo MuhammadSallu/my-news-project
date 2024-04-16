0bbf528484473a1e8f1bf5646f964a808f7fab1b
Momo Sallu <MuhammadSallu@gmail.com>
2024-04-16 16:17:20 +0100

    Retrieved comments by article id

--- model/app.model.js
+++ model/app.model.js
@@ -31,4 +31,17 @@ const selectArticles = () => {
     });
 };
 
-module.exports = { selectTopics, selectArticleById, selectArticles };
+const selectCommentsByArticleId = (article_id) => {
+  return db
+    .query(`SELECT * FROM comments WHERE article_id = $1;`, [article_id])
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
+module.exports = {
+  selectTopics,
+  selectArticleById,
+  selectArticles,
+  selectCommentsByArticleId,
+};
